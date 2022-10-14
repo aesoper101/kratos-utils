5fd4563bf9aaca81fbeaacf7d9f5e6912a4d1f29
aesoper <weilanzhuan@163.com>
2022-10-15 00:18:03 +0800

    feat: add tlsconfig

---
+++ pkg/tls.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"crypto/tls"
+	"github.com/aesoper101/kratos-utils/protobuf/types/confpb"
+)
+
+func InitTLSConfig(cfg *confpb.TLSConfig) *tls.Config {
+	if cfg != nil && cfg.GetEnabled() {
+		cert, err := tls.LoadX509KeyPair(cfg.GetCertFile(), cfg.GetKeyFile())
+		if err != nil {
+			panic(err)
+		}
+		return &tls.Config{Certificates: []tls.Certificate{cert}}
+	}
+
+	return nil
+}
