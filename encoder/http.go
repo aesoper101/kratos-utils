201b61e577bc701300f3393588b88805e147cee9
aesoper <weilanzhuan@163.com>
2022-10-16 11:07:40 +0800

    feat: add function `ApiEncodeErrorEncoder`

--- encoder/http.go
+++ encoder/http.go
@@ -4,7 +4,7 @@ import (
 	"github.com/aesoper101/kratos-utils/middleware/requestid"
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	http2 "net/http"
+	stbhttp "net/http"
 	"strings"
 	"time"
 )
@@ -18,29 +18,29 @@ func contentType(subtype string) string {
 	return strings.Join([]string{baseContentType, subtype}, "/")
 }
 
+type response struct {
+	// 错误码，跟 http-status 一致，并且在 grpc 中可以转换成 grpc-status
+	Code int32 `json:"code"`
+	// 错误原因，定义为业务判定错误码
+	Reason string `json:"reason,omitempty"`
+	// 错误信息，为用户可读的信息，可作为用户提示内容
+	Message   string      `json:"message"`
+	Data      interface{} `json:"data,omitempty"`
+	RequestId string      `json:"requestId"`
+	Time      int64       `json:"time"`
+	// 错误元信息，为错误添加附加可扩展信息
+	Metadata map[string]string `json:"metadata,omitempty"`
+}
+
 func ApiEncodeResponse() http.EncodeResponseFunc {
-	return func(w http2.ResponseWriter, r *http2.Request, v interface{}) error {
+	return func(w stbhttp.ResponseWriter, r *stbhttp.Request, v interface{}) error {
 		if v == nil {
 			return nil
 		}
 		if rd, ok := v.(http.Redirector); ok {
 			url, code := rd.Redirect()
-			http2.Redirect(w, r, url, code)
+			stbhttp.Redirect(w, r, url, code)
 			return nil
-		}
-
-		type response struct {
-			// 错误码，跟 http-status 一致，并且在 grpc 中可以转换成 grpc-status
-			Code int32 `json:"code"`
-			// 错误原因，定义为业务判定错误码
-			Reason string `json:"reason,omitempty"`
-			// 错误信息，为用户可读的信息，可作为用户提示内容
-			Message   string      `json:"message"`
-			Data      interface{} `json:"data,omitempty"`
-			RequestId string      `json:"requestId"`
-			Time      int64       `json:"time"`
-			// 错误元信息，为错误添加附加可扩展信息
-			Metadata map[string]string `json:"metadata,omitempty"`
 		}
 
 		reply := &response{
@@ -80,5 +80,37 @@ func ApiEncodeResponse() http.EncodeResponseFunc {
 			return err
 		}
 		return nil
+	}
+}
+
+func ApiEncodeErrorEncoder() http.EncodeErrorFunc {
+	return func(w http.ResponseWriter, r *http.Request, err error) {
+		se := errors.FromError(err)
+
+		reply := &response{
+			Code:    se.GetCode(),
+			Time:    time.Now().Unix(),
+			Message: se.GetMessage(),
+			Reason:  se.GetReason(),
+		}
+
+		if m := se.GetMetadata(); m != nil {
+			reply.Metadata = m
+		}
+
+		requestId := w.Header().Get(requestid.HeaderRequestId)
+		if len(requestId) > 0 {
+			reply.RequestId = requestId
+		}
+
+		codec, _ := http.CodecForRequest(r, "Accept")
+		body, err := codec.Marshal(reply)
+		if err != nil {
+			w.WriteHeader(stbhttp.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", contentType(codec.Name()))
+		w.WriteHeader(int(se.Code))
+		_, _ = w.Write(body)
 	}
 }
