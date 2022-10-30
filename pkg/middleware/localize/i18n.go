7f482bd190da3f14609d96aa1fdd10c7e8247ee3
aesoper <weilanzhuan@163.com>
2022-10-30 12:24:19 +0800

    refactor: do refactor I18nBundle

--- pkg/middleware/localize/i18n.go
+++ pkg/middleware/localize/i18n.go
@@ -4,27 +4,26 @@ import (
 	"embed"
 	"encoding/json"
 	"github.com/BurntSushi/toml"
+	"github.com/aesoper101/go-utils/templatex"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 	"gopkg.in/yaml.v3"
 	"path/filepath"
+	"sync"
 	"text/template"
 )
 
 type (
 	I18nBundle struct {
-		Bundle *i18n.Bundle
+		bundle *i18n.Bundle
+		reMux  sync.RWMutex
+		funcs  template.FuncMap
 	}
 
 	Localizer struct {
 		bundle    *I18nBundle
 		localizer *i18n.Localizer
-	}
-
-	Config struct {
-		PluralCount interface{}
-
-		Funcs template.FuncMap
+		funcs     template.FuncMap
 	}
 )
 
@@ -44,7 +43,10 @@ func NewBundleFromEmbedFs(fs embed.FS, pattern string) (*I18nBundle, error) {
 		}
 	}
 
-	return &I18nBundle{Bundle: bundle}, nil
+	return &I18nBundle{
+		bundle: bundle,
+		funcs:  templatex.DefaultTemplateFuncMap,
+	}, nil
 }
 
 func NewBundleFromFile(pattern string) (*I18nBundle, error) {
@@ -63,11 +65,23 @@ func NewBundleFromFile(pattern string) (*I18nBundle, error) {
 		}
 	}
 
-	return &I18nBundle{Bundle: bundle}, nil
+	return &I18nBundle{
+		bundle: bundle,
+		funcs:  templatex.DefaultTemplateFuncMap,
+	}, nil
 }
 
 func (b *I18nBundle) RegisterUnmarshalFunc(format string, unmarshalFunc i18n.UnmarshalFunc) {
-	b.Bundle.RegisterUnmarshalFunc(format, unmarshalFunc)
+	b.bundle.RegisterUnmarshalFunc(format, unmarshalFunc)
+}
+
+func (b *I18nBundle) RegisterFuncs(funcs template.FuncMap) {
+	b.reMux.Lock()
+	defer b.reMux.Unlock()
+
+	for k, v := range funcs {
+		b.funcs[k] = v
+	}
 }
 
 func setDefaultUnmarshalFunc(bundle *i18n.Bundle) {
@@ -77,22 +91,23 @@ func setDefaultUnmarshalFunc(bundle *i18n.Bundle) {
 }
 
 func NewLocalizer(bundle *I18nBundle, langs ...string) *Localizer {
-	localizer := i18n.NewLocalizer(bundle.Bundle, langs...)
+	localizer := i18n.NewLocalizer(bundle.bundle, langs...)
 
 	return &Localizer{
 		localizer: localizer,
+		funcs:     bundle.funcs,
 	}
 }
 
-func (l *Localizer) T(messageId string, data interface{}, args ...Config) string {
+func (l *Localizer) T(messageId string, data interface{}, pluralCounts ...interface{}) string {
 	cfg := &i18n.LocalizeConfig{
 		MessageID:    messageId,
 		TemplateData: data,
+		Funcs:        l.funcs,
 	}
 
-	if len(args) > 0 {
-		cfg.PluralCount = args[0].PluralCount
-		cfg.Funcs = args[0].Funcs
+	if len(pluralCounts) > 0 {
+		cfg.PluralCount = pluralCounts[0]
 	}
 
 	msg, err := l.localizer.Localize(cfg)
